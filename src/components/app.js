c4e544b780596cb8cd440b9ba7aeddfe2e45aa0d
Minh Pham <minhpham@myhost.local>
2017-11-06 09:01:18 +0700

    3 - List Item Keys

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './search_bar';
+import VideoList from './video_list';
 
 const API_KEY = 'AIzaSyAjP07KG9IoX47GBwpLRy3qff6QqVe0HKc';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-      	<SearchBar/>
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+
+		this.state = { videos: []};
+
+		YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+			// this.setState({videos: videos});
+			this.setState({videos});
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);
+	}
 }
